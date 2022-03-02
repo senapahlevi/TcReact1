072c40686e3ae4f1b16cf21930f5324a9398aaf3
senapahlevi <senapahlevi1@gmail.com>
2022-03-02 12:34:15 +0700

    delete all unused file and code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
-// import { Ads } from "./ads";
-// import { Articles } from "./articles";
-// import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import { ButtonBase, CardContent, IconButton, Typography } from "@mui/material";
-import CardMedia from "@mui/material/CardMedia";
-// import FavoriteIcon from "@mui/icons-material/Favorite";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import React from "react";
+
+import { IconButton, Typography } from "@mui/material";
+
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
@@ -15,7 +9,7 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import Adsj from "./adsj.json";
 import articlesj from "./articlesj.json";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 const Home = () => {
   const Json1 = [...articlesj];
@@ -31,14 +25,14 @@ const Home = () => {
     <div className="container-fluid">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="#Home">
             H
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
+            <div className="navbar-nav align-self-end">
               <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
@@ -46,13 +40,13 @@ const Home = () => {
           </div>
         </div>
       </nav>
-      <div className="row justify-content-center rounded-4">
+      <div className="row   rounded-4" id="Home">
         {newArrs.map((item, key) => {
           return (
-            <div className="mt-4 col-sm-2 col-lg-4 col-12 " key={key}>
+            <div className=" mt-4 col-sm-6 col-lg-4 col-12 " key={key}>
               <Card style={{ width: "20rem", height: 660 }}>
                 <Card.Img variant="top" src={item.cover + "/" + key} />
-                <Card.Body>
+                <Card.Body className="text-start">
                   <Card.Title> {item.title}</Card.Title>
                   <Card.Text> {item.summary}</Card.Text>
                 </Card.Body>
@@ -80,31 +74,3 @@ const Home = () => {
 };
 
 export default Home;
-
-/*
- <Card sx={{ maxWidth: 430, height: 660 }} className="col-12 mb-3 mr-3">
-                <CardMedia component="img" height="260" image={item.cover + "/" + key} alt="cover" />
-                <CardContent className="">
-                  <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left" }}>
-                    {item.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left" }}>
-                    {item.summary}
-                  </Typography>
-                  <div className="row mt-3">
-                    <div className="col mt-2 ">{key % 2 === 0 ? "" : <h6 className="text-success">Sponsored Ads</h6>}</div>
-                    <div className="col">
-                      <IconButton aria-label="add to favorites">
-                        <Typography variant="caption" color="text.secondary">
-                          {item.likes}
-                        </Typography>
-                        <FavoriteBorderOutlinedIcon />
-                        {item.bookmarked === false ? <LocalOfferOutlinedIcon /> : <LocalOfferIcon />}
-                        <ShareOutlinedIcon />
-                      </IconButton>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
-
-*/
